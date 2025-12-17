328aeed6bc604846b4902c98c924c3166a0544bb
Bia262 <vargab4@kkszki.hu>
2025-12-17 08:12:34 +0100

    xxx

--- Bevasarlolista/MainWindow.xaml.cs
+++ Bevasarlolista/MainWindow.xaml.cs
@@ -14,11 +14,43 @@ namespace Bevasarlolista
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+   
+
+        public class ItemModel
+        {
+            public String Nev {  get; set; }
+            public int Mennyiseg { get; set; }
+            public int Ar {  get; set; }
+            public String Kategoria { get; set; }
+            public int Osszesen { get; set; }
+
+            public ItemModel(string nev, int mennyiseg, int ar, string kategoria, int osszesen)
+            {
+                Nev = nev;
+                Mennyiseg = mennyiseg;
+                Ar = ar;
+                Kategoria = kategoria;
+                Osszesen = Mennyiseg*Ar;
+            }
+        }
     public partial class MainWindow : Window
     {
+        List<ItemModel> termekek = new List<ItemModel>();
+
         public MainWindow()
         {
             InitializeComponent();
+            termekek.Add(new ItemModel("Tej", 5, 450, "A"));
+            termekek.Add(new ItemModel("Kenyer", 10, 350, "B"));
+            termekek.Add(new ItemModel("Sajt", 2, 1200, "A"));
+            termekek.Add(new ItemModel("Alma", 20, 200, "C"));
+            termekek.Add(new ItemModel("Narancs", 15, 300, "C"));
+            termekek.Add(new ItemModel("Hús", 3, 2500, "D"));
+            termekek.Add(new ItemModel("Csokoládé", 7, 900, "B"));
+            termekek.Add(new ItemModel("Kenyér", 1, 450, "B"));
+            termekek.Add(new ItemModel("Tej", 12, 400, "A"));
+            termekek.Add(new ItemModel("Sajt", 5, 1500, "D"));
+            dataGrid.ItemsSource = termekek;
         }
     }
 }
